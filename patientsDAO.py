f9b32b56420ffda07c3169f8d91d4d229b01ce92
Ger Hanlon <G000364757gmit.ie>
2019-12-22 14:57:29 +0000

    Update Project Content

---
+++ patientsDAO.py
@@ -0,0 +1,70 @@
+import mysql.connector
+class StudentDAO:
+    db=""
+    def __init__(self):
+        self.db = mysql.connector.connect(
+        host="localhost",
+        port="3306",
+        user="root",
+        password="kim123",
+    #user="gerardh",
+    #password="Nov2019123"
+        database="patients"
+        )
+    def create(self, values):
+        cursor = self.db.cursor()
+        sql="insert into patients (Name, Symptoms, DoctorId) values (%s, %s, %s)"
+
+        cursor.execute(sql, values)
+        self.db.commit()
+        return cursor.lastrowid
+    
+    def getAll(self):
+        cursor = self.db.cursor()
+        sql="select * from patients"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        print(results)
+        for result in results:
+            returnArray.append(self.convertToDictionary(result))
+        return returnArray
+    
+    def findByID(self, id):
+        cursor = self.db.cursor()
+        sql = "select * from patients where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        return self.convertToDictionary(result)
+
+    def update(self, values):
+        cursor = self.db.cursor()
+        sql = "update patients set Name= %s, Symptoms=%s, DoctorId where id = %s"
+        cursor.execute(sql, values)
+        self.db.commit()
+    
+    def delete(self, id):
+        cursor = self.db.cursor()
+        sql="delete from patients where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+
+        self.db.commit()
+        print("delete done")
+
+    def convertToDictionary(self, result):
+        colnames=['id', 'Name', 'Symptoms', "DoctorId"]
+
+        item = {}
+
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                item[colName] = value
+            
+        return item
+
+patientsDAO = patientsDAO()
--- server.py
+++ server.py
@@ -1,86 +1,84 @@
 from flask import Flask, jsonify, request, abort
 import requests 
 import json
+from patientsDAO import patientsDAO
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
-patients=[
-    {"id":1, "Name": "Ger", "Symptoms": "Achy Breaky Heart", "DoctorId":101},
-    {"id":2, "Name": "Bob", "Symptoms": "Ingrown Toe Nail", "DoctorId":102},
-    {"id":3, "Name": "Andrew", "Symptoms": "Tennis Elbow", "DoctorId":103},
-    ]
-nextID=4
-
+#patients=[
+ #   {"id":1, "Name": "Ger", "Symptoms": "Achy Breaky Heart", "DoctorId":101},
+  #  {"id":2, "Name": "Bob", "Symptoms": "Ingrown Toe Nail", "DoctorId":102},
+   # {"id":3, "Name": "Andrew", "Symptoms": "Tennis Elbow", "DoctorId":103},
+    #]
+#nextID=4
 
  #"http://127.0.0.1:5000/hospital2"
 @app.route('/hospital2')
 def getAll():
-     return jsonify(patients)
+     results = patientsDAO.getAll()
+
+     return jsonify(results)
 
 #curl "http://127.0.0.1:5000/hospital/2"
 @app.route('/hospital2/<int:id>')
 def findByID(id):
-    foundPatients = list(filter(lambda t: t['id']== id, patients))
-    if len(foundPatients)== 0:
-        return jsonify({}), 204
+    foundPatients = patientsDAO.findByID(id)
 
-    return jsonify(foundPatients[0])
+    return jsonify(foundPatients)
 
 #curl -i -H "Content-Type:application/json" -X POST -d "{\"Name\":\"Mary\",\"Symptoms\":\"Fiddlers Elbow\",\"DoctorId\":104}" http://127.0.0.1:5000/hospital2
 @app.route('/hospital2', methods=['POST'])
 def create():
-    global nextID
     if not request.json:
         abort(400)
         
     patient = {
-        "id": nextID,
         "Name": request.json['Name'],
         "Symptoms": request.json['Symptoms'],
         "DoctorId": request.json['DoctorId'],
         }
-    nextID += 1
-    patients.append(patient)
+    values = (patient['Name'], patient['Symptoms'], patient['DoctorId'])
+    newId = patientsDAO.create(values)
+    book['id'] = newId
     return jsonify(patient)
 
 #curl -i -H "Content-Type:application/json" -X PUT -d "{\"Name\":\"Mary\",\"Symptoms\":\"Broken Arm\",\"DoctorId\":104}" http://127.0.0.1:5000/hospital2
 @app.route('/hospital2/<int:id>', methods=['PUT'])
 def update(id):
-    foundpatients = list(filter(lambda t: t['id']== id, patients))
-    if (len(foundpatients)==0):
+    foundpatients = patientsDAO.findByID(id)
+    if not foundpatients:
         abort(404)
-        foundPatient = foundpatients[0]
-        if not request.json:
-            abort(400)
-        reqJson = request.json
-        if 'DoctorId' in reqJson and type(reqJson['DoctorId']) is not int:
-            abort(400)
-        if 'Name' in reqJson:
-            foundPatient['Name'] = reqJson['Name']    
-        if 'Symptoms' in reqJson:
-            foundPatient['Symptoms'] = reqJson['Symptoms']
-        if 'DoctorId' in reqJson:
-            foundPatient['DoctorId'] = reqJson['DoctorId']
-    return jsonify(foundPatient)
+        
+    if not request.json:
+        abort(400)
+    reqJson = request.json
+    if 'DoctorId' in reqJson and type(reqJson['DoctorId']) is not int:
+        abort(400)
 
-    return "in update for id"+str(id)
+    if 'Name' in reqJson:
+        foundPatients['Name'] = reqJson['Name']    
+    if 'Symptoms' in reqJson:
+        foundPatients['Symptoms'] = reqJson['Symptoms']
+    if 'DoctorId' in reqJson:
+        foundPatients['DoctorId'] = reqJson['DoctorId']
+    values = (foundpatients['Name'], foundpatients['Symptoms'], foundpatients['DoctorId'], foundpatients['id'])
+    patientsDAO.update(values)
+    return jsonify(foundpatients)
+
 
 #curl -X DELETE "http://127.0.0.1:5000/hospital1/1"
 @app.route('/hospital2/<int:id>', methods=['DELETE'])
 def delete(id):
-    foundPatients = list(filter(lambda t: t['id']== id, patients))
-    if (len(foundPatients) == 0):
-        abort(404)
-    patients.remove(foundPatients[0])
+    patientsDAO.delete(id)
     return jsonify({"done":True})
 
-@app.errorhandler(404)
-def not_found404(error):
-    return make_response( jsonify( {'error':'Not found' }), 404)
-
-@app.errorhandler(400)
-def not_found400(error):
-    return make_response( jsonify( {'error':'Bad Request' }), 400)
+#@app.errorhandler(404)
+#def not_found404(error):
+#    return make_response( jsonify( {'error':'Not found' }), 404)
+#
+#@app.errorhandler(400)
+#def not_found400(error):
+#    return make_response( jsonify( {'error':'Bad Request' }), 400)
 
 if __name__=='__main__':
     app.run(debug=True)
